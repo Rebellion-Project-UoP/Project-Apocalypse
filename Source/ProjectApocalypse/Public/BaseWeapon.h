54dda71f77c7e6abd947340966e086b5bcf85380
Jacob Kelly <up651292@myport.ac.uk>
2023-11-07 15:02:23 +0000

    created a base weapon bp to be expanded upon, moved files around to tidy project up and fixed up redirectories

---
+++ Source/ProjectApocalypse/Public/BaseWeapon.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseWeapon.generated.h"
+
+UCLASS()
+class PROJECTAPOCALYPSE_API ABaseWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseWeapon();
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Ammo")
+	int Ammunition;
+	
+	int MaxAmmunition;
+	int Clip;
+	
+	float Recoil;
+	float Accuracy;
+	
+	bool bIsProjectile;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
