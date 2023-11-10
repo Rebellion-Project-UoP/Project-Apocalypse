13e0393f11fad297b246027dc55b416f563b0429
Cgcam2001 <49098734+Cgcam2001@users.noreply.github.com>
2023-11-10 14:51:05 +0000

    ai controller being used

--- Source/ProjectApocalypse/Private/ZombieBase.cpp
+++ Source/ProjectApocalypse/Private/ZombieBase.cpp
@@ -4,6 +4,7 @@
 #include "ZombieBase.h"
 #include "AIController.h"
 #include "NavigationSystem.h"
+#include "Kismet/GameplayStatics.h"
 
 
 
@@ -21,21 +22,32 @@ void AZombieBase::BeginPlay()
 	Super::BeginPlay();
 
 	_aiController = Cast<AAIController>(GetController());
+	
+	FTimerHandle UnusedHandle;
+
+	GetWorldTimerManager().SetTimer(UnusedHandle, this, &AZombieBase::Move, 4, true, 0);
+	
+}
+
+
+void AZombieBase::Move()
+{
 	FNavLocation result;
 
 	UNavigationSystemV1* navData = UNavigationSystemV1::GetCurrent(GetWorld());
-	navData->GetRandomReachablePointInRadius(GetActorLocation(), radius, result);
+	navData->GetRandomReachablePointInRadius(GetActorLocation() , radius, result);
+	
+	_aiController->MoveToLocation(result.Location, (-1.0f), false);
 
-	_aiController->MoveToLocation(result.Location, 5, false);
+	//_aiController->MoveToActor(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+
+	
+	
+	
 
 
-	//_aiController->GetPathFollowingComponent()->OnRequestFinished.AddUObject(this, &AZombieBase::OnMoveComplete);
 }
 
-//UPathFollowingComponent::FMoveComplete AZombieBase::OnMoveComplete(FAIRequestID requestID, const FPathFollowingResult& result)
-//{
-//	return;
-//}
 
 // Called every frame
 void AZombieBase::Tick(float DeltaTime)
@@ -44,12 +56,14 @@ void AZombieBase::Tick(float DeltaTime)
 
 }
 
+
 // Called to bind functionality to input
 void AZombieBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
 
 
 
