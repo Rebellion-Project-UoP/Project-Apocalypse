54abd1c4550b213f349c783375c2454709ba4321
Cgcam2001 <49098734+Cgcam2001@users.noreply.github.com>
2023-11-27 17:03:15 +0000

    wall avoidance working for multiple line traces

--- Source/ProjectApocalypse/Private/WallAvoidance.cpp
+++ Source/ProjectApocalypse/Private/WallAvoidance.cpp
@@ -8,31 +8,53 @@
 FVector UWallAvoidance::Calculate()
 {
 
+	FVector steeringForce = FVector::Zero();
+
 	FHitResult hit;
 
 	float penDistance = 0;
 
 	FVector location = GetOwner()->GetActorLocation();
 	FVector fwdVector = GetOwner()->GetActorForwardVector();
-	
-	if (GetWorld()->LineTraceSingleByChannel(hit, location,location + fwdVector * viewDistance, ECollisionChannel::ECC_Visibility)) {
-	DrawDebugLine(GetWorld(), location, location + fwdVector * viewDistance, hit.bBlockingHit ? FColor::Blue : FColor::Red, false, 5.0f, 0, 5.0f);
+
+	if (GetWorld()->LineTraceSingleByChannel(hit, location, location + fwdVector * viewDistance, ECollisionChannel::ECC_Visibility)) {
 
 
 
-		if (!(hit.GetComponent()->ComponentHasTag("Wall"))) {
-			return  FVector::Zero();
+		if (hit.GetComponent()->ComponentHasTag("Wall")) {
+		
+			penDistance = FVector::Distance(hit.TraceEnd, hit.TraceEnd - hit.ImpactPoint);
+
+
+			steeringForce += (hit.Normal * penDistance);
 		}
-
-		penDistance = FVector::Distance(hit.TraceEnd, hit.TraceEnd - hit.ImpactPoint);
-
-		GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1.0f, FColor::Red, FString::SanitizeFloat(penDistance));
-
-		return (hit.Normal * penDistance) * 5;
 	}
 
-	
+	FHitResult hitRight;
 
-	return FVector::Zero();
+	if (GetWorld()->LineTraceSingleByChannel(hitRight, location, location + (fwdVector + GetOwner()->GetActorRightVector()) * (viewDistance /3), ECollisionChannel::ECC_Visibility)) {
+		if (hitRight.GetComponent()->ComponentHasTag("Wall")) {
+			penDistance = FVector::Distance(hitRight.TraceEnd, hitRight.TraceEnd - hitRight.ImpactPoint);
+
+
+
+			steeringForce += (hitRight.Normal * penDistance);
+		}
+	}
+
+	FHitResult hitLeft;
+
+	if (GetWorld()->LineTraceSingleByChannel(hitLeft, location, location + (fwdVector - GetOwner()->GetActorRightVector()) * (viewDistance / 3), ECollisionChannel::ECC_Visibility)) {
+		if (hitLeft.GetComponent()->ComponentHasTag("Wall")) {
+			penDistance = FVector::Distance(hitLeft.TraceEnd, hitLeft.TraceEnd - hitLeft.ImpactPoint);
+
+
+			steeringForce += (hitLeft.Normal * penDistance);
+
+		}
+	}
+
+
+	return steeringForce * 6;
 }
 
