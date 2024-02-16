4460c6ff91d7e552cb1ea178f8f9da1a5040234a
Cgcam2001 <49098734+Cgcam2001@users.noreply.github.com>
2024-02-16 16:40:36 +0000

    added blood splat material

--- Source/ProjectApocalypse/Private/ZombieBase.cpp
+++ Source/ProjectApocalypse/Private/ZombieBase.cpp
@@ -19,7 +19,7 @@ FVector AZombieBase::getSteeringVelocity()
 AZombieBase::AZombieBase()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	
+
 	PrimaryActorTick.bCanEverTick = true;
 
 	aggroRange = 2000.0f;
@@ -46,23 +46,23 @@ void AZombieBase::BeginPlay()
 	swipeHitBox->OnComponentBeginOverlap.AddDynamic(this, &AZombieBase::OnBoxBeginOverlap);
 
 	neighbourhood = GetComponentByClass<UNeighbourhoodRadius>();
-	
+
 	healthComponent = Cast<UHealthComp>(GetComponentByClass(UHealthComp::StaticClass()));
-	
+
 	//gets the behaviours attached to the zombie
 	TArray<UActorComponent*> behavioursFound = GetComponentsByClass(USteeringBehaviour::StaticClass());
 
-	for(int i = 0; i < behavioursFound.Num(); i++ )
+	for (int i = 0; i < behavioursFound.Num(); i++)
 	{
 		USteeringBehaviour* b = Cast<USteeringBehaviour>(behavioursFound[i]);
 		_behaviours.Add(b);
-		
+
 	};
-	
+
 	//FTimerHandle UnusedHandle;
 	//GetWorldTimerManager().SetTimer(UnusedHandle, this, &AZombieBase::MoveToPlayer, 2, true, 0);
 
-	
+
 	//_skeletalMesh = Cast<USkeletalMeshComponent>(this->GetComponentByClass(USkeletalMeshComponent::StaticClass));
 
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Block);
@@ -78,12 +78,27 @@ void AZombieBase::Tick(float DeltaTime)
 
 	if (!playerDetected) {
 
+
+		if (GetVelocity().Length() > maxWalkSpeed) {
+
+			GetCharacterMovement()->MaxWalkSpeed -= 2;
+
+			if (GetVelocity().Length() <= maxWalkSpeed) {
+
+				GetCharacterMovement()->MaxWalkSpeed = maxWalkSpeed;
+			}
+		}
+
+
 		SetActorRotation(UKismetMathLibrary::RInterpTo_Constant(GetActorRotation(), _velocity.ToOrientationRotator(), UGameplayStatics::GetWorldDeltaSeconds(GetWorld()), 80));
-	
+
 
 
 
 		Move();
+	}
+	else {
+		GetCharacterMovement()->MaxWalkSpeed = maxRunSpeed;
 	}
 
 
@@ -119,9 +134,11 @@ void AZombieBase::Move()
 
 	FVector accel = steeringForce / 1;
 	_velocity = accel * UGameplayStatics::GetWorldDeltaSeconds(GetWorld());
-	
 
-	_velocity.GetClampedToSize(0,GetCharacterMovement()->MaxWalkSpeed);
+
+	_velocity = _velocity.GetClampedToSize(0, GetCharacterMovement()->MaxWalkSpeed);
+
+
 
 	if (_velocity != FVector::Zero()) {
 		_velocity.Z = 0;
