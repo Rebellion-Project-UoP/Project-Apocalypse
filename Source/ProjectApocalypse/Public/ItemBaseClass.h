d8aca670d5d7186ad0e82d0f05f8ab384f45a223
AdenBryan <adenhbryan@gmail.com>
2024-02-23 16:58:15 +0000

    minor saves before turning off
    No real changes just small tweaks

--- Source/ProjectApocalypse/Public/ItemBaseClass.h
+++ Source/ProjectApocalypse/Public/ItemBaseClass.h
@@ -31,4 +31,6 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
 };
