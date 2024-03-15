d535c38933c912cbc40266518b9dc0a490f4a00f
Cgcam2001 <49098734+Cgcam2001@users.noreply.github.com>
2024-03-15 21:19:14 +0000

    fixed

--- Source/ProjectApocalypse/Private/ZombieBase.cpp
+++ Source/ProjectApocalypse/Private/ZombieBase.cpp
@@ -121,7 +121,7 @@ void AZombieBase::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor*
 
 			UHealthComp* health = player->GetComponentByClass<UHealthComp>();
 
-			IDamageInterface::Execute_TakeDamage(health, damage);
+			IDamageInterface::Execute_TakeDamage(health, 20);
 		}
 	}
 }
--- Source/ProjectApocalypse/Public/ZombieBase.h
+++ Source/ProjectApocalypse/Public/ZombieBase.h
@@ -36,9 +36,6 @@ public:
 		UPROPERTY(EditAnywhere, BlueprintReadWrite)
 			float maxRunSpeed;
 
-		UPROPERTY(EditAnywhere, BlueprintReadWrite)
-			float damage;
-
 	UHealthComp* healthComponent;
 
 	bool hasPointsBeenReceived;
