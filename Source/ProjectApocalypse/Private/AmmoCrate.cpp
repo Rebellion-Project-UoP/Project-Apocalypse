c5f06f53c44a9c9e44c2f74e66b042c164fd6c65
AdenBryan <adenhbryan@gmail.com>
2024-03-18 15:41:07 +0000

    Loading and saving of the score now works and need to work on the widget to display the highscores

--- Source/ProjectApocalypse/Private/AmmoCrate.cpp
+++ Source/ProjectApocalypse/Private/AmmoCrate.cpp
@@ -13,9 +13,10 @@ void AAmmoCrate::UseAction(AProjectApocalypseCharacter* player)
 {	
 	if (player->AmmoCrateReserves > 0)
 	{
-		if (player->weaponRef->Ammunition != player->weaponRef->MaxAmmunition)
+		if (player->weaponRef->Ammunition < player->weaponRef->MaxAmmunition)
 		{
-			player->weaponRef->Ammunition += 30;
+			//player->weaponRef->Ammunition += 30;
+			player->weaponRef->Ammunition = player->weaponRef->MaxAmmunition;
 
 			if (player->weaponRef->Ammunition > player->weaponRef->MaxAmmunition)
 			{
